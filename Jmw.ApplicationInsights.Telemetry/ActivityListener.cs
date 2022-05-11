8f679d378fce9d2ac6a27551f7ccc9e42a39c951
Jean-Marc Weeger <jmweeger@ugc.fr>
2022-05-11 17:04:07 +0200

    feat(telemetry): adding MySQLConnectorListener.

---
+++ Jmw.ApplicationInsights.Telemetry/ActivityListener.cs
@@ -0,0 +1,128 @@
+﻿// <copyright file="ActivityListener.cs" company="Jean-Marc Weeger">
+// Copyright My Company under MIT Licence. See https://opensource.org/licenses/mit-license.php.
+// </copyright>
+
+namespace Jmw.ApplicationInsights.Telemetry
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using Dawn;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+    /// <summary>
+    /// .Net listener mapping an activity to an Application Insights Telemetry.
+    /// </summary>
+    /// <typeparam name="TTelemetry">Type of Application Insight Telemetry.</typeparam>
+    public class ActivityListener<TTelemetry> : IDisposable
+        where TTelemetry : OperationTelemetry, new()
+    {
+        private readonly TelemetryClient telemetryClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActivityListener{TTelemetry}"/> class.
+        /// </summary>
+        /// <param name="telemetryClient">Instance of the telemery client.</param>
+        /// <param name="activityFilter">Callback filtering the activity to monitor.</param>
+        public ActivityListener(TelemetryClient telemetryClient, Func<ActivitySource, bool> activityFilter)
+        {
+            this.telemetryClient = Guard.Argument(telemetryClient, nameof(telemetryClient)).NotNull();
+            Guard.Argument(activityFilter, nameof(activityFilter)).NotNull();
+
+            this.ActivitySourceListener = new ActivityListener()
+            {
+                ActivityStopped = activity => this.OnActivityStopped(activity),
+                ShouldListenTo = activityFilter,
+                SampleUsingParentId = (ref ActivityCreationOptions<string> _) => ActivitySamplingResult.AllData,
+                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData,
+            };
+        }
+
+        /// <summary>
+        /// Gets the instance of the activity listener to be added with <see cref="ActivitySource.AddActivityListener(ActivityListener)"/>.
+        /// </summary>
+        public ActivityListener ActivitySourceListener { get; }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.ActivitySourceListener.Dispose();
+        }
+
+        /// <summary>
+        /// Function allowing to enrich telemetry with custom data.
+        /// </summary>
+        /// <param name="telemetry">Telemetry to enrich.</param>
+        /// <param name="sourceActivity">Source activity.</param>
+        protected virtual void EnrichTelemetry(TTelemetry telemetry, Activity sourceActivity)
+        {
+        }
+
+        private static TTelemetry ActivityToTelemetry(Activity activity)
+        {
+            // From Application Insights code source
+            var telemetry = new TTelemetry();
+            telemetry.Name = activity.OperationName;
+            telemetry.Timestamp = activity.StartTimeUtc;
+            telemetry.Duration = activity.Duration;
+            OperationContext operation = telemetry.Context.Operation;
+            operation.Name = activity.OperationName;
+            if (activity.IdFormat == ActivityIdFormat.W3C)
+            {
+                operation.Id = activity.TraceId.ToHexString();
+                telemetry.Id = activity.SpanId.ToHexString();
+                if (string.IsNullOrEmpty(operation.ParentId) && activity.ParentSpanId != default(ActivitySpanId))
+                {
+                    operation.ParentId = activity.ParentSpanId.ToHexString();
+                }
+            }
+            else
+            {
+                operation.Id = activity.RootId;
+                operation.ParentId = activity.ParentId;
+                telemetry.Id = activity.Id;
+            }
+
+            foreach (KeyValuePair<string, string> item in activity.Baggage)
+            {
+                if (!telemetry.Properties.ContainsKey(item.Key))
+                {
+                    telemetry.Properties.Add(item);
+                }
+            }
+
+            foreach (KeyValuePair<string, string> tag in activity.Tags)
+            {
+                if (!telemetry.Properties.ContainsKey(tag.Key))
+                {
+                    telemetry.Properties.Add(tag);
+                }
+            }
+
+            return telemetry;
+        }
+
+        private void OnActivityStopped(Activity activity)
+        {
+            if (this.telemetryClient.IsEnabled())
+            {
+                var telemetry = ActivityToTelemetry(activity);
+
+                if (telemetry is DependencyTelemetry)
+                {
+                    this.telemetryClient.TrackDependency(telemetry as DependencyTelemetry);
+                }
+                else if (telemetry is MetricTelemetry)
+                {
+                    this.telemetryClient.TrackMetric(telemetry as MetricTelemetry);
+                }
+                else if (telemetry is EventTelemetry)
+                {
+                    this.telemetryClient.TrackEvent(telemetry as EventTelemetry);
+                }
+            }
+        }
+    }
+}
---
+++ Jmw.ApplicationInsights.Telemetry/MySQLConnectorListener.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="MySQLConnectorListener.cs" company="Jean-Marc Weeger">
+// Copyright My Company under MIT Licence. See https://opensource.org/licenses/mit-license.php.
+// </copyright>
+
+namespace Jmw.ApplicationInsights.Telemetry
+{
+    using System;
+    using System.Diagnostics;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    /// <summary>
+    /// MySQL Connector Activity listener. See https://mysqlconnector.net/tutorials/tracing/.
+    /// </summary>
+    public class MySQLConnectorListener : ActivityListener<DependencyTelemetry>
+    {
+        /// <summary>
+        /// MySQLConnector Actity Name.
+        /// </summary>
+        public const string ActivityName = "MySQLConnector";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySQLConnectorListener"/> class.
+        /// </summary>
+        /// <param name="telemetryClient">Instance of the telemery client.</param>
+        public MySQLConnectorListener(TelemetryClient telemetryClient)
+            : base(telemetryClient, s => s.Name == ActivityName)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void EnrichTelemetry(DependencyTelemetry telemetry, Activity sourceActivity)
+        {
+            base.EnrichTelemetry(telemetry, sourceActivity);
+
+            string dbName = string.Empty;
+            string dbServer = "localhost";
+            string dbServerPort = "3306";
+            string command = sourceActivity.OperationName;
+
+            foreach (var baggage in sourceActivity.Tags)
+            {
+                switch (baggage.Key)
+                {
+                    case "db.statement":
+                        command = baggage.Value;
+                        break;
+                    case "db.name":
+                        dbName = baggage.Value;
+                        break;
+                    case "net.peer.name":
+                        dbServer = baggage.Value;
+                        break;
+                    case "net.peer.ip":
+                        dbServer = baggage.Value;
+                        break;
+                    case "net.peer.port":
+                        dbServerPort = baggage.Value;
+                        break;
+                }
+            }
+
+            telemetry.Type = "SQL";
+            telemetry.Target = $"mysql:{dbServer},{dbServerPort} | {dbName}";
+            telemetry.Data = command;
+        }
+    }
+}
